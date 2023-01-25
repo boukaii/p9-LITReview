8a630771e75015f614c686b80e446df681af9a31
guillaume71 <guillaume271293@hotmail.fr>
2023-01-25 18:42:15 +0100

    APP

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,8 @@ from . import models
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput,
+                               label='Mot de passe')
 
 
 class TicketForm(forms.ModelForm):
@@ -14,8 +15,10 @@ class TicketForm(forms.ModelForm):
 
 
 class ReviewForm(forms.ModelForm):
-    choice_value = [('1', '- 1'), ('2', '- 2'), ('3', '- 3'), ('4', '- 4'), ('5', '- 5')]
-    rating = forms.ChoiceField(label='Note', widget=forms.RadioSelect, choices=choice_value)
+    choice_value = [('1', '- 1'), ('2', '- 2'), ('3', '- 3'),
+                    ('4', '- 4'), ('5', '- 5')]
+    rating = forms.ChoiceField(label='Note', widget=forms.RadioSelect,
+                               choices=choice_value)
     edit_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
     class Meta:
--- blog/models.py
+++ blog/models.py
@@ -19,13 +19,15 @@ class User(AbstractUser):
 class Ticket(models.Model):
     titre = models.CharField(max_length=128)
     description = models.TextField(max_length=2048, blank=True)
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
 
 
 class Review(models.Model):
-    ticket = models.ForeignKey(Ticket, null=True, on_delete=models.SET_NULL, blank=True)
+    ticket = models.ForeignKey(Ticket, null=True,
+                               on_delete=models.SET_NULL, blank=True)
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
         validators=[MinValueValidator(0), MaxValueValidator(5)])
